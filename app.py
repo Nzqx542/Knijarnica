7541a1808c19a1c4d165060380893a7de6bfdd71
Nzqx542 <nt43245754@edu.mon.bg>
2026-02-16 12:22:51 +0200

    Create app.py

---
+++ app.py
@@ -0,0 +1,21 @@
+import streamlit as st
+books = [
+  "The Hobbit",
+  "1984",
+  "Pride and Prejudice",
+  "To Kill a Mockingbird",
+  "The great Gatsby",
+  "Withering Heights"
+]
+st.title("book Checker app")
+st.write("Enter a book title to check if it exists in the database")
+
+user_input = st.text_input("Book Title: ")
+
+if st.button("Check Book"):
+  if user_input.strip() == "":
+    st.warning("Please enter a book title.")
+  elif user_input in books: 
+    st.success("The book exists in the database!")
+  else:
+    st.error("The book is not in the database!")
